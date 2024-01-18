c15795d7181aa3fdf671b24542fe9b25761275f5
luyanfei <luyanfei78@163.com>
2024-01-18 10:13:09 +0000

    add nginx-simple.

---
+++ clear_iptables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+iptables -F && iptables -X
+iptables -t nat -F && iptables -t nat -X
+iptables -t raw -F && iptables -t raw -X
+iptables -t mangle -F && iptables -t mangle -X
+systemctl restart containerd
+systemctl restart kubelet
+
+
